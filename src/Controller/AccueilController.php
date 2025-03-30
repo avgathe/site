c4e423ad6b03f05b73d06b0bd3a21b06ff47ac16
Agathe <vautier.agathe@gmail.com>
2025-03-30 18:01:29 +0200

    [fix] bug security

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,21 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Security\Core\Security;
+
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil_index')]
-    public function index(Security $security): Response
+    public function index(): Response
     {
 
-        $user = $security->getUser();
-        $roles = $user ? $user->getRoles() : ['ROLE_VISITEUR'];
-
-        return $this->render('index.html.twig', [
-            'roles' => $roles,
-        ]);
+        return $this->render('Accueil/index.html.twig');
     }
+
+//    #[Route('/', name: 'accueil_index')]
+//    public function index(Security $security): Response
+//    {
+//
+//        $user = $security->getUser();
+//        $roles = $user ? $user->getRoles() : ['ROLE_VISITEUR'];
+//
+//        return $this->render('index.html.twig', [
+//            'roles' => $roles,
+//        ]);
+//    }
 
 
     public function menuAction(): Response
