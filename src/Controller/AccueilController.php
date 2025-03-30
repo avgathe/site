64724beb0eb92e4ffab297fa06e6a7620a026380
Agathe <vautier.agathe@gmail.com>
2025-03-30 16:10:03 +0200

    [feat]controller accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'accueil_index')]
+    public function indexAction(): Response
+    {
+        return $this->render('Accueil/index.html.twig');
+    }
+
+}
