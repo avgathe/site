53098ce6e3a10f20ea89fbb01e50f6ef11ac56a2
Agathe <vautier.agathe@gmail.com>
2025-03-30 18:04:34 +0200

    [fix] affichage role page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,31 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\Security\Core\Security;
 
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil_index')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
 
-        return $this->render('Accueil/index.html.twig');
-    }
+        $user = $security->getUser();
+        $roles = $user ? $user->getRoles() : ['ROLE_VISITEUR'];
 
-//    #[Route('/', name: 'accueil_index')]
-//    public function index(Security $security): Response
-//    {
-//
-//        $user = $security->getUser();
-//        $roles = $user ? $user->getRoles() : ['ROLE_VISITEUR'];
-//
-//        return $this->render('index.html.twig', [
-//            'roles' => $roles,
-//        ]);
-//    }
+        return $this->render('Accueil/index.html.twig', [
+            'roles' => $roles,
+        ]);
+    }
 
 
     public function menuAction(): Response
