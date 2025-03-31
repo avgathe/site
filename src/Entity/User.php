2a6b98a633e92fa4269435aa1b653a03be765a9e
Agathe <vautier.agathe@gmail.com>
2025-03-31 13:26:59 +0200

    [update]on peut s'authentifier

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -96,9 +96,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_NOROLE';
 
-        return array_unique($roles);
+        return array_values(array_unique($roles));
     }
 
     /**
